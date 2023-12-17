07d16aa7bc825e1c4c51b86fb0c2472d54d6e708
alex <enna@mailbox.org>
2023-12-17 11:05:36 +0100

    Add pausable

--- src/Wrt.sol
+++ src/Wrt.sol
@@ -1,23 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-// Import the ERC20 interface and implementation from OpenZeppelin
 import "@openzeppelin-contracts@5.0.1/token/ERC20/ERC20.sol";
 import "@openzeppelin-contracts@5.0.1/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin-contracts@5.0.1/access/Ownable.sol";  // Import the Ownable contract
+import "@openzeppelin-contracts@5.0.1/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin-contracts@5.0.1/access/Ownable.sol";
 
-contract Wrt is ERC20, ERC20Burnable, Ownable {  // Add Ownable to the inheritance list
-    // The constructor sets up the new token
+contract Wrt is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
     constructor(string memory name, string memory symbol, uint256 initialSupply)
     ERC20(name, symbol)
-    Ownable(msg.sender)  // Pass the deployer's address to the Ownable constructor
+    Ownable(msg.sender)
     {
-        // Mint the initial supply to the address that deploys the contract
         _mint(msg.sender, initialSupply);
     }
 
-    // Example of a function that only the owner can call
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    // Correctly override the _update function from ERC20Pausable
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, amount);
+    }
+
+    // Function to pause the contract
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    // Function to unpause the contract
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }
--- test/Wrt.t.sol
+++ test/Wrt.t.sol
@@ -3,16 +3,19 @@ pragma solidity 0.8.21;
 
 import "ds-test/test.sol";
 import "../src/Wrt.sol";
+import "forge-std/Test.sol";
 
-contract WrtTest is DSTest {
+contract WrtTest is DSTest, Test  {
     Wrt wrt;
     address deployer;
+    address nonOwner;
 
     function setUp() public {
         deployer = address(this);
-        // Assuming the initial supply is 1000 tokens
+        nonOwner = address(0x2);
         wrt = new Wrt("WRT Token", "WRT", 1000);
     }
+
 
     function testInitialBalance() public {
         // Check if the deployer's balance is equal to the initial supply
@@ -34,4 +37,61 @@ contract WrtTest is DSTest {
         // Check if the deployer is the owner
         assertEq(wrt.owner(), deployer);
     }
+
+    function testPauseFunctionality() public {
+        // Ensure only owner can pause and unpause
+        wrt.pause();
+        assertTrue(wrt.paused());
+
+        wrt.unpause();
+        assertTrue(!wrt.paused());
+    }
+
+    function testFailTransferWhenPaused() public {
+        uint256 transferAmount = 50;
+        address recipient = address(0x123);
+
+        wrt.pause();
+        wrt.transfer(recipient, transferAmount); // This should fail
+    }
+
+    function testFailMintWhenPaused() public {
+        uint256 mintAmount = 50;
+
+        wrt.pause();
+        wrt.mint(deployer, mintAmount); // This should fail
+    }
+
+    function testFailBurnWhenPaused() public {
+        uint256 burnAmount = 50;
+
+        wrt.pause();
+        wrt.burn(burnAmount); // This should fail
+    }
+
+
+    // Test mint function
+    function testMint() public {
+        uint256 mintAmount = 100;
+        wrt.mint(deployer, mintAmount);
+        assertEq(wrt.totalSupply(), 1100);
+        assertEq(wrt.balanceOf(deployer), 1100);
+    }
+
+    // Test failure to pause/unpause from non-owner
+    function testFailPauseByNonOwner() public {
+        vm.prank(nonOwner);
+        wrt.pause();
+    }
+
+    function testFailUnpauseByNonOwner() public {
+        wrt.pause();
+        vm.prank(nonOwner);
+        wrt.unpause();
+    }
+
+
+
+
+
 }
